3b35f166773049297350f02c2e036fe849608697
Home <home@Kias-iMac.local>
2023-12-18 21:59:54 +0330

    farsi

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function Form() {
   }
 
   return /*#__PURE__*/(
-    React.createElement("div", { className: "bg-white p-6 rounded-2xl w-max" }, /*#__PURE__*/
+    React.createElement("div", { className: "bg-white border p-6 rounded-2xl w-max" }, /*#__PURE__*/
     React.createElement("div", { className: "flex gap-8" }, /*#__PURE__*/
     React.createElement(Step, { step: step, stepOrder: 1, setStep: setStep }), /*#__PURE__*/
     React.createElement(Step, { step: step, stepOrder: 2, setStep: setStep }), /*#__PURE__*/
@@ -34,19 +34,51 @@ function Form() {
     React.createElement(Step, { step: step, stepOrder: 4, setStep: setStep })),
 
     step === 1 && /*#__PURE__*/
-    React.createElement("div", null,
-    " ", /*#__PURE__*/
-    React.createElement("div", { className: "h-2 w-full rounded-lg mt-8" }, "\u0644\u0637\u0641\u0627 \u0634\u0645\u0627\u0631\u0647 \u062A\u0644\u0641\u0646 \u0648\u0627\u0631\u062F \u06A9\u0646\u06CC\u062F."),
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("div", { className: "h-2 w-full rounded-lg mt-8" }, "\u0644\u0637\u0641\u0627 \u0634\u0645\u0627\u0631\u0647 \u062A\u0644\u0641\u0646 \u0648\u0627\u0631\u062F \u06A9\u0646\u06CC\u062F."), /*#__PURE__*/
 
-    " "),
+
+    React.createElement("input", { className: "border w-full rounded-lg mt-8" }), /*#__PURE__*/
+    React.createElement("div", { className: "h-4 w-full rounded-lg mt-4" }, "\u0628\u0631\u0631\u0633\u06CC \u06A9\u0646\u06CC\u0645 \u062F\u0631 \u062F\u0633\u062A\u0631\u0633\u0647\u061F")),
+
+
+
+
+    step === 2 && /*#__PURE__*/
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("div", { className: "h-2 w-full rounded-lg mt-8" }, "\u0686\u0642\u062F\u0631 \u0622\u0634\u063A\u0627\u0644 \u062F\u0627\u0631\u06CC\u062F\u061F"), /*#__PURE__*/
+    React.createElement("div", { className: "flex justify-between mt-8" }, /*#__PURE__*/
+    React.createElement("button", {
+      onClick: nextStep,
+      className: "border bg-white text-black py-2 px-8 rounded-full" }, "\u0628\u0632\u0631\u06AF"), /*#__PURE__*/
+
+
+
+    React.createElement("button", {
+      onClick: nextStep,
+      className: "border bg-white text-black py-2 px-8 rounded-full" }, "\u0628\u0632\u0631\u06AF"))),
+
+
+
+
+
+
+    step === 3 && /*#__PURE__*/
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("div", { className: "h-2 w-full rounded-lg mt-8" }, "\u0644\u0637\u0641\u0627 \u0634\u0645\u0627\u0631\u0647 \u062A\u0644\u0641\u0646 \u0648\u0627\u0631\u062F \u06A9\u0646\u06CC\u062F."), /*#__PURE__*/
+
+
+    React.createElement("input", { className: "border w-full rounded-lg mt-8" }), /*#__PURE__*/
+    React.createElement("div", { className: "h-4 w-full rounded-lg mt-4" }, "\u0628\u0631\u0631\u0633\u06CC \u06A9\u0646\u06CC\u0645 \u062F\u0631 \u062F\u0633\u062A\u0631\u0633\u0647\u061F")),
+
+
 
 
     step === 4 && /*#__PURE__*/
     React.createElement("div", null, /*#__PURE__*/
     React.createElement("div", { className: "h-8 w-full bg-gray-100 rounded-lg mt-8" }), /*#__PURE__*/
     React.createElement("div", { className: "h-4 w-full bg-gray-100 rounded-lg mt-4" }), /*#__PURE__*/
-    React.createElement("div", { className: "h-4 w-3/4 bg-gray-100 rounded-lg mt-2" }), /*#__PURE__*/
-    React.createElement("div", { className: "h-4 w-1/2 bg-gray-100 rounded-lg mt-2" })), /*#__PURE__*/
+    React.createElement("div", { className: "h-4 w-3/4 bg-gray-100 rounded-lg mt-2" })), /*#__PURE__*/
 
 
     React.createElement("div", { className: "flex justify-between mt-8" }, /*#__PURE__*/
@@ -89,9 +121,22 @@ function Step({ step, stepOrder, setStep }) {
       transition: { duration: 0.5 },
       className: "w-12 h-12 rounded-full border fill-white text-gray-500 grid place-items-center" },
 
-    status === "complete" ? /*#__PURE__*/React.createElement(CheckIcon, null) : /*#__PURE__*/React.createElement("span", null, stepOrder)));
+    status === "complete" ? /*#__PURE__*/
+    React.createElement(CheckIcon, null) : /*#__PURE__*/
+
+    React.createElement("span", null, toFarsiNumber(stepOrder))));
 
 
+
+}
+function toFarsiNumber(n) {
+  const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+  return n.
+  toString().
+  split("").
+  map(x => farsiDigits[x]).
+  join("");
 }
 
 function CheckIcon() {
